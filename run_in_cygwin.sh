cd0b585924dbff6847f04248ab6cf5f90a1a0552
Revanth <rvnthvrm@gmail.com>
2020-04-19 09:58:41 +0530

    Update run_in_cygwin.sh

--- run_in_cygwin.sh
+++ run_in_cygwin.sh
@@ -2,12 +2,12 @@
 DIR="/cygdrive/c/beat/"
 
 if [ -d "$DIR" ]; then
-  rm -f /cygdrive/c/beat/ && mkdir /cygdrive/c/beat
+  rm -rf /cygdrive/c/beat/ && mkdir /cygdrive/c/beat
 else
   mkdir /cygdrive/c/beat
 fi
 
 cd /cygdrive/c/beat/ &&
-mkdir beat-virtual-env && cd beat-virtual-env && pip3 install virtualenv && virtualenv . && . bin/activate && cd .. && git clone https://github.com/rvnthvrm/beat.git &&
-cd beat && pip3 install -r requirments.txt &&
+mkdir beat-virtual-env && cd beat-virtual-env && pip3.8 install virtualenv && virtualenv . && . bin/activate && cd .. && git clone https://github.com/rvnthvrm/beat.git &&
+cd beat && pip3.8 install -r requirments.txt &&
 flask run
